841f2fb1476b0ea4fd1240d8f11ac1208a55b130
Rut <vanrut042@gmail.com>
2024-11-23 16:04:45 +0200

    added logo

--- src/Components/PagesArea/Projects/Projects.tsx
+++ src/Components/PagesArea/Projects/Projects.tsx
@@ -17,13 +17,19 @@ export function Projects(): JSX.Element {
                     siteLink="https://weather-rut.web.app/home"
                     githubLink="https://github.com/Rut96/react-weather-app"
                     title="Weather App"
-                    description ="Full-stack React application showcasing advanced weather visualization.
-                    Created responsive UI with dynamic weather animations and glass-morphic design. Implemented real-time data 
-                    fetching, error handling, and 7-day forecasting. Built with React, TypeScript and Weather 
-                    API integration. Feel free to check out the live demo or code on my GitHub."
+                    description={[
+                        "Full-stack React application showcasing advanced weather visualization.",
+                        "Created responsive UI with dynamic weather animations and glass-morphic design.",
+                        "Implemented real-time data fetching, error handling, and 7-day forecasting.",
+                        "Built with React, TypeScript and Weather API integration."
+                    ]}
+                    // description ="Full-stack React application showcasing advanced weather visualization.
+                    // Created responsive UI with dynamic weather animations and glass-morphic design. Implemented real-time data 
+                    // fetching, error handling, and 7-day forecasting. Built with React, TypeScript and Weather 
+                    // API integration. Feel free to check out the live demo or code on my GitHub."
                     technologies={["React", "TypeScript", "CSS", "Weather API Integration"]} />
-                <ProjectCard image={defaultImg} title="Project 2" description="Project description" />
-                <ProjectCard image={defaultImg} title="Project 3" description="Project description" />
+                <ProjectCard image={defaultImg} title="Project 2" description={["Project description"]} />
+                <ProjectCard image={defaultImg} title="Project 3" description={["Project description"]} />
             </div>
         </div>
     );
